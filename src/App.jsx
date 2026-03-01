62ce0ba7b3e5516d95217865ec14e052623d8ac5
unknown <yad.4322@gmail.com>
2026-03-02 01:12:11 +0300

    startings

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./assets/App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <>
+      {/* Upper navbar */}
+      <div>
+        <div>
+          <div>
+            <a href=""></a>
+          </div>
+        </div>
+      </div>
+      {/* lower navbar */}
+      <div>Navbar</div>
+    </>
+  );
+}
